c1e9330d23a7ad31888b28114a2f2f47f82631a9
Kindeep Singh Kargil <kindeepsingh@gmail.com>
2021-04-11 15:08:34 -0400

    Cool graph

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./App.css";
 import jsonp from "jsonp";
 import useVaccineData from "./data/useVaccineData";
+import { Line } from "react-chartjs-2";
 
 function App() {
   const { data, derived } = useVaccineData();
@@ -13,12 +14,66 @@ function App() {
     );
   }, []);
 
+  const chartData = {
+    labels: data.map((d) => d.report_date.toDateString()),
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: derived.at_least_1_ratio
+      }
+    ]
+  };
+
+  // const chartData = {
+  //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  //   datasets: [
+  //     {
+  //       label: 'My First dataset',
+  //       fill: false,
+  //       lineTension: 0.1,
+  //       backgroundColor: 'rgba(75,192,192,0.4)',
+  //       borderColor: 'rgba(75,192,192,1)',
+  //       borderCapStyle: 'butt',
+  //       borderDash: [],
+  //       borderDashOffset: 0.0,
+  //       borderJoinStyle: 'miter',
+  //       pointBorderColor: 'rgba(75,192,192,1)',
+  //       pointBackgroundColor: '#fff',
+  //       pointBorderWidth: 1,
+  //       pointHoverRadius: 5,
+  //       pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+  //       pointHoverBorderColor: 'rgba(220,220,220,1)',
+  //       pointHoverBorderWidth: 2,
+  //       pointRadius: 1,
+  //       pointHitRadius: 10,
+  //       data: [65, 59, 80, 81, 56, 55, 40]
+  //     }
+  //   ]
+  // };
+
   return (
     <div>
-      <pre>{JSON.stringify(derived, null, 2)}</pre>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Line data={chartData}></Line>
     </div>
   );
 }
 
 export default App;
+ 
